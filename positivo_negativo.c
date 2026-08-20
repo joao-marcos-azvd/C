7f009db96bb82b700b4076917844468d4b09d970
João Marcos <122325944+joao-marcos-azvd@users.noreply.github.com>
2026-08-20 11:15:35 -0300

    Código em C - Aprendendo!

---
+++ positivo_negativo.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+                            --- Positivo ou negativo --- 
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("--- Positivo ou negativo --- \n");
+    printf("Digite um valor: ");
+    scanf("%d", &num);
+    if (num > 0) {
+        printf("O valor %d é POSITIVO!", num);
+    }
+    else {
+        printf("O valor %d é NEGATIVO!", num);
+    }
+
+    return 0;
+}
