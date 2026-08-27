a4600f3952fb75e1f6c38747f7da4d62f05e1d79
João Marcos <122325944+joao-marcos-azvd@users.noreply.github.com>
2026-08-27 12:12:12 -0300

    Fix logical errors and enhance comments in quadratic equation
    Corrected logical errors in conditions and improved comments.

--- Q05_equacao_segundo_grau_turbinada.c
+++ Q05_equacao_segundo_grau_turbinada.c
@@ -1,7 +1,7 @@
 /* Equação do 2° Grau */
 
 #include <stdio.h>
-# include <math.h> // Importando uma biblioteca matemática
+# include <math.h> // Importando uma biblioteca matemática (sqrt(), pow() e fabs())
 
 int main()
 {
@@ -24,21 +24,20 @@ int main()
     
     printf("\nEquação: %.1fX² + %.1fx + %.1f \n", a, b, c);
     
-    // Condições para x1 e x2 - Números Reais
+    // Condições para x1 e x2
     if (delta >= 0)
         {
-            if (delta = 0) // Tem 2 raizes iguais
-                {
+            if (delta == 0) // Tem 2 raizes iguais
+                {   
                     float x;
                     x = (-1 * b / (2*a));
                 
                     printf("\n2 Raizes iguais: \n");
                     printf("X1: %.1f | X2: %.1f ", x, x);
-                    
                 }
-        
+                
             else  // Tem 2 raizes distintas
-                {   
+                {
                     float x1, x2;
                     // Calculando X1 (+):
                     x1 = (-1 * b + sqrt(delta)) / (2*a);
@@ -50,18 +49,18 @@ int main()
                     printf("X1: %.1f | X2: %.1f ", x1, x2);
                 }   
         }
-
-    // Números complexos
+    
     else  // Delta < 0
         {
             float real, imganiraia; 
             real = (-1 * b) / (2*a); // Pegando a parte real do número
-            imganiraia = sqrt(fabs(delta)) / 2 * a; // Pegando a paret imaginária do número
+            imganiraia = sqrt(fabs(delta)) / (2 * a); // Pegando a paret imaginária do número
             
             printf("\nRaizes complexas conjulgadas:\n");
             printf("\n - (%.2f)/2 * %.2f +- j(%.2f**1/2)/2 * %.2f\n", b, a, delta, a);
             printf("\nParte real: %.2f | Parte Imaginária: j%.2f ", real, imganiraia);
         }
+    
 
     return 0;
 }
