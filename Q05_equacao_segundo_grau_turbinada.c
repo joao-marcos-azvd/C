9a32f6d368ecf1bb8a1eb6f61578d5da60cdb0c1
João Marcos <122325944+joao-marcos-azvd@users.noreply.github.com>
2026-08-27 11:43:43 -0300

    Refactor quadratic equation logic and comments
    Corrected comments and fixed logic for equal roots condition.

--- Q05_equacao_segundo_grau_turbinada.c
+++ Q05_equacao_segundo_grau_turbinada.c
@@ -1,4 +1,4 @@
-/* Equação do 2° Grau Turbinada*/
+/* Equação do 2° Grau */
 
 #include <stdio.h>
 # include <math.h> // Importando uma biblioteca matemática
@@ -24,11 +24,21 @@ int main()
     
     printf("\nEquação: %.1fX² + %.1fx + %.1f \n", a, b, c);
     
-    // Condições para x1 e x2
+    // Condições para x1 e x2 - Números Reais
     if (delta >= 0)
         {
-            if (delta > 0) // Tem 2 raizes distintas
+            if (delta = 0) // Tem 2 raizes iguais
                 {
+                    float x;
+                    x = (-1 * b / (2*a));
+                
+                    printf("\n2 Raizes iguais: \n");
+                    printf("X1: %.1f | X2: %.1f ", x, x);
+                    
+                }
+        
+            else  // Tem 2 raizes distintas
+                {   
                     float x1, x2;
                     // Calculando X1 (+):
                     x1 = (-1 * b + sqrt(delta)) / (2*a);
@@ -38,30 +48,20 @@ int main()
                     
                     printf("\n2 Raizes distintas: \n");
                     printf("X1: %.1f | X2: %.1f ", x1, x2);
-                }
-        
-            else  // Tem 2 raizes iguais
-                {   
-                    float x;
-                    x = (-1 * b / (2*a));
-                
-                    printf("\n2 Raizes iguais: \n");
-                    printf("X1: %.1f | X2: %.1f ", x, x);
                 }   
         }
-    
+
+    // Números complexos
     else  // Delta < 0
         {
             float real, imganiraia; 
             real = (-1 * b) / (2*a); // Pegando a parte real do número
             imganiraia = sqrt(fabs(delta)) / 2 * a; // Pegando a paret imaginária do número
             
-            // 
             printf("\nRaizes complexas conjulgadas:\n");
             printf("\n - (%.2f)/2 * %.2f +- j(%.2f**1/2)/2 * %.2f\n", b, a, delta, a);
             printf("\nParte real: %.2f | Parte Imaginária: j%.2f ", real, imganiraia);
         }
-    
 
     return 0;
 }
